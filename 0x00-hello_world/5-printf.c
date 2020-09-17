c030ce102dd2e1ca9d3593958596535b59851dc8
Luisa Uribe <luribeasprilla@gmail.com>
2020-09-17 00:30:37 +0000

    C program that prints a sentence using the printf function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - C program that prints a sentence using the printf function, followed by a new line.
+ *
+ * Return: Always (0).
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+		return (0);
+}
