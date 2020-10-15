722392323de8cbdafe8517f959f458954fd030c0
Luisa Uribe <luribeasprilla@gmail.com>
2020-10-15 23:39:12 +0000

    Function that checks for alphabetic character

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+
+/**
+ * _isalpha - checks for alphabetic character.
+ * @c: character to test.
+ * Return: 1 if c is is a letter lowercase or uppercase, 0 otherwise.
+ */
+
+int _isalpha(int c)
+{
+
+
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+
+	}
+	else if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+
+	}
+
+
+}
