0b2289ccb5cd00ce9efafbbc954537651d420d57
luisauribe <luribeasprilla@gmail.com>
2020-10-24 16:09:30 -0500

    function that prints the numbers, from 0 to 9 without 2 and 9

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - function that prints the numbers,
+ * from 0 to 9 whitout, followed by a new line.
+ * Return: void.
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (i != '2' && i != '4')
+		_putchar(i);
+	}
+
+	_putchar('\n');
+}
