aa5d5b15e6faec15a84fa714fb1d43f66035f3a1
luisauribe <luribeasprilla@gmail.com>
2020-10-25 01:21:04 -0500

    Function that prints a square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -2,6 +2,7 @@
 
 /**
  * print_square - function that prints a square, followed by a new line
+ * @size: size of square.
  * Return: void.
  */
 
@@ -17,7 +18,7 @@ void print_square(int size)
 
 	for (a = 0; a < size; a++)
 	{
-	        for (b = 0; b < size; b++)
+		for (b = 0; b < size; b++)
 		{
 			_putchar('#');
 		}
