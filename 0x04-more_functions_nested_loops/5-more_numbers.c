4cdee962e42add37ad8e942339f842f310a5d3a7
luisauribe <luribeasprilla@gmail.com>
2020-10-24 23:18:30 -0500

    _putchar 3 times

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,16 +6,12 @@
  */
 void more_numbers(void)
 {
-	int a, b;
+	int b;
 
-	for (a = 0; a <= 9; a++)
+	for (b = 0; b <= 14; b++)
 	{
-		_putchar(a + '0');
-	}
-
-	for (b = 10; b <= 14; b++)
-	{
-		_putchar((b / 10) + '0');
+		if (b > 9)
+			_putchar((b / 10) + '0');
 		_putchar((b % 10) + '0');
 	}
 	_putchar ('\n');
