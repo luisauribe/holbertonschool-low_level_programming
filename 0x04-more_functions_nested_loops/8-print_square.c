7639a800cb67e9f66d7ece05f44af722eb469004
luisauribe <luribeasprilla@gmail.com>
2020-10-25 00:36:24 -0500

    Function that prints a square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_square - function that prints a square, followed by a new line
+ * Return: void.
+ */
+
+void print_square(int size)
+{
+	int a, b;
+
+	for (a = 0; a < size; a++)
+	{
+		for (b = 0; b < size; b++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
