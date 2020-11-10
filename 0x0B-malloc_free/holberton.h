dc4930356283d18dde500d401f076c156fc03d5e
luisauribe <luribeasprilla@gmail.com>
2020-11-10 16:43:54 +0000

    holberton.h

---
+++ 0x0B-malloc_free/holberton.h
@@ -0,0 +1,27 @@
+#ifndef HOLBERTON
+#define HOLBERTON
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(char c);
+
+char *_strcpy(char *dest, char *src);
+
+int _strlen(char *s);
+
+char *create_array(unsigned int size, char c);
+
+char *_strdup(char *str);
+
+char *str_concat(char *s1, char *s2);
+
+int **alloc_grid(int width, int height);
+
+void free_grid(int **grid, int height);
+
+char *argstostr(int ac, char **av);
+
+char **strtow(char *str);
+
+#endif
