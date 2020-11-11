c18f3be05b0c5672603f18cb8d87274c49550515
luisauribe <luribeasprilla@gmail.com>
2020-11-11 01:29:51 +0000

    Test file.

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -25,9 +25,9 @@ int main(int argc, char *argv[])
 
 	if (argv[2][1] != 0 ||
 	    (argv[2][0] != '+' &&
-	         argv[2][0] != '-' &&
-	         argv[2][0] != '*' &&
-	         argv[2][0] != '/' &&
+	     argv[2][0] != '-' &&
+	     argv[2][0] != '*' &&
+	     argv[2][0] != '/' &&
 	     argv[2][0] != '%'))
 	{
 		printf("Error\n");
