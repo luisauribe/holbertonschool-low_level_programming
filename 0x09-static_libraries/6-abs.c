f9ad18ec5aafb5ece891084fa98cb1b5a5580783
luisauribe <luribeasprilla@gmail.com>
2020-11-28 01:12:05 +0000

    Functions contained in the static library.

---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _abs - main function.
+ * @i: Test character.
+ *
+ * Description: This function computes the absolute value of an integer.
+ * Return: 0.
+ */
+
+int _abs(int i)
+{
+	return (i > 0 ? i : -i);
+}
+© 2020 GitHub, Inc.
